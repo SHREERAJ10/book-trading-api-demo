ccdbc935f2d2d72faa0e9ef147d1d5020048657b
Shreeraj Shrestha <shreerajshrestha.dev@gmail.com>
2025-08-18 19:34:33 +0545

    added endpoint to get all books data

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+
+app.get("/books", async(req, res)=>{
+    const data = await prisma.book.findMany({
+        select:{
+            id:true,
+            name:true,
+            author:true
+        }
+    });
+    res.json(data);
+});
 
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
